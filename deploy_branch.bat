537bffecf7584fb62243a3a09ccdf76dace4df84
MoRDekai1364 <159567358+MoRDekai1364@users.noreply.github.com>
2026-07-13 21:33:54 +0300

    added code files folder

--- deploy_branch.bat
+++ deploy_branch.bat
@@ -1,13 +1,20 @@
 @echo off
 setlocal
 
-
 set /p msg="Enter commit message: "
 
+:: Handle submodule if it exists and has changes
+if exist "code_files\.git" (
+    echo [INFO] Detected submodule. Checking for changes...
+    cd code_files
+    git add .
+    git commit -m "Auto-commit: %msg%"
+    cd ..
+    git add code_files
+)
 
+:: Stage and commit main repo
 git add .
-
-
 git commit -m "%msg%"
 
 :: Push to the currently checked-out branch
